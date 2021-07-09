275565ff77d900d0fd9ff4eb2ed58cd4401b42d7
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-09 13:03:25 -0600

    routing created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import Space from './Pages/Space';
+import Drinks from './Pages/Drinks';
 
 function App() {
   return (
-    <>hello</>
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/space' component={Space} />
+        <Route path='/drinks' component={Drinks} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
