4ae37558bbcc7b95d61a0cd58018d26755a580a1
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-18 09:15:54 -0600

    fixed styling problem

--- src/Components/space/CarouselInSpace.js
+++ src/Components/space/CarouselInSpace.js
@@ -3,7 +3,7 @@ import SpaceImg from '../../assets/Space.jpg'
 
 export default function CarouselView({ spacePeople }) {
   const galaxyPeoples = spacePeople.people.map((peoples, index) => (
-    <Carousel.Item key={index} style={{ 'height':'80px', 'display':'flex', 'align-items':'center'}}>
+    <Carousel.Item key={index} style={{ 'height':'80px', 'display':'flex', 'alignItems':'center'}}>
       <img
         style={{'width':'100%'}}
         className="d-block w-100"
@@ -11,7 +11,7 @@ export default function CarouselView({ spacePeople }) {
         alt="First slide"
       />
       <Carousel.Caption style={{'height':'80px','bottom':'0px' }}>
-        <p style={{'margin':'0px', 'font-size':'13px'}}>People currently in space!</p>
+        <p style={{'margin':'0px', 'fontSize':'13px'}}>People currently in space!</p>
         <p style={{'margin':'0px'}}>{peoples.name}</p>
       </Carousel.Caption>
     </Carousel.Item>
