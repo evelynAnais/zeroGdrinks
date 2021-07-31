7ecab008a4fb7ce682021ff6ffa1148bd1f24b2c
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-31 16:54:38 -0600

    added more words to story

--- src/Components/home/Story.js
+++ src/Components/home/Story.js
@@ -1,5 +1,5 @@
 import { Card } from 'react-bootstrap'
-import { randomWord, weatherRandom, timeOfDayRandom, timeRandom, sizeRandom, beautyRandom, timesRandom } from "../../utils/randomWords"
+import { randomWord, weatherRandom, timeOfDayRandom, timeRandom, sizeRandom, beautyRandom, timesRandom, partyRandom, sizesRandom, amazingRandom, unexpectedRandom } from "../../utils/randomWords"
 
 export default function Story({ drink, spaceObject, story }) {
   return (
@@ -13,13 +13,12 @@ export default function Story({ drink, spaceObject, story }) {
             decided to call { story.friend }. They were going to have a party even though it had 
             been { randomWord(timeRandom) } since anyone had been on earth. Such a { randomWord(sizeRandom) } planet
             compared to the { randomWord(beautyRandom) } planet they now lived on. They called up all their friends 
-            and {story?.number} showed up. It was amazing. They drank { drink?.strDrink } and danced to {story?.artist}.
-            They were having so much fun that they attracted some unexpected guests! { randomWord(sizeRandom) } {story?.color} aliens
-            with big {story?.bodyPart} who ended up being the life of the party. { story.friend } gave {story?.idol} the alien
-            a { drink?.strDrink } and a {story?.object}. They were having so much fun! Everyone was cheering them on to
-            keep the party going. They partied for { randomWord(timeRandom) }. Everyone decided they needed to make it a
-            tradition to celebrate how great life was on {spaceObject?.name} with their new alien friends! So 
-            every {story?.number} { randomWord(timesRandom) } they celebrate with { drink?.strDrink } for { randomWord(timeRandom) }!
+            and {story?.number} showed up. It was a { randomWord(partyRandom) }. They drank { drink?.strDrink } and danced to { story?.artist }.
+            They were having so much fun that they attracted some { randomWord(unexpectedRandom) } guests! Aliens, { randomWord(sizeRandom) } { story?.color } aliens
+            with { randomWord(sizesRandom) } {story?.bodyPart} who ended up being the life of the party. { story.friend } gave { story?.idol } the alien
+            a { drink?.strDrink } and a {story?.object}. They were having so much fun! They partied for { randomWord(timeRandom) }. Everyone 
+            decided they needed to make it a tradition to celebrate how { randomWord(amazingRandom) } life was on {spaceObject?.name} with their new alien friends! So 
+            every {story?.number} { randomWord(timesRandom) } they celebrate with { drink?.strDrink } and { story?.idol }!
           </p>
         </Card.Body>
       </Card>
--- src/utils/randomWords.js
+++ src/utils/randomWords.js
@@ -1,9 +1,13 @@
-export const weatherRandom = ['sunny', 'cloudy', 'rainy', 'snowy', 'foggy', 'stormy', 'bright', 'brilliant', 'astronomical', 'starry']
+export const weatherRandom = ['sunny', 'cloudy', 'rainy', 'snowy', 'foggy', 'stormy', 'bright', 'brilliant', 'astronomical', 'magical', 'starry']
 export const timeOfDayRandom = ['morning', 'midnight', 'noon', 'night', 'evening']
 export const timeRandom = ['months', 'days', 'years', 'millennia', 'weeks', 'centuries', 'decades']
-export const sizeRandom = ['big', 'gigantic', 'tiny', 'small', 'minuscule', 'compact', 'dainty', 'colossal', 'immense', 'momentous', 'massive' ]
+export const sizeRandom = ['big', 'measly', 'expansive', 'gigantic', 'tiny', 'small', 'minuscule', 'compact', 'dainty', 'colossal', 'immense', 'momentous', 'massive', 'puny', 'vast', 'hulking']
+export const sizesRandom = ['big', 'gigantic', 'tiny', 'small', 'minuscule', 'compact', 'dainty', 'colossal', 'immense', 'momentous', 'massive', 'galactic', 'microscopic', 'petite', 'jumbo']
 export const beautyRandom = ['brilliant', 'dazzling', 'lustrous', 'luminescent', 'gleaming', 'astute', 'beaming', 'luminous']
-export const timesRandom = ['months', 'days', 'years', 'millennia', 'weeks', 'centuries', 'decades']
+export const timesRandom = ['months', 'days', 'weeks']
+export const partyRandom = ['gathering', 'party', 'ball', 'fiesta', 'celebration', 'reunion', 'blowout', 'banquet', 'bash']
+export const amazingRandom = ['amazing', 'astonishing', 'astounding', 'breathtaking', 'perplexing', 'remarkable', 'wonderful', 'amazeballs', 'flabbergasting', 'mind-blowing']
+export const unexpectedRandom = ['unexpected', 'unpredicted', 'unforeseen', 'serendipitous', 'extraordinary', 'unplanned', 'surprising']
 
 
 export function randomWord(list) {
