3d508a248cb2c80b8793cac1034a045e8247a276
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-31 13:14:19 -0600

    added function for random words

--- src/utils/randomWords.js
+++ src/utils/randomWords.js
@@ -1,5 +1,21 @@
-const verbsRandom = ['run', 'jump']
-const nounsRandom = []
-const adjectivesRandom = []
-const timeOfDayRandom = ['morning', 'midnight', 'noon', 'night', 'evening']
+export const verbsRandom = ['run', 'jump']
+export const nounsRandom = []
+export const adjectivesRandom = []
+export const timeOfDayRandom = ['morning', 'midnight', 'noon', 'night', 'evening']
+export const weatherRandom = ['sunny', 'cloudy', 'rainy', 'snowy', 'foggy', 'stormy', 'bright', 'brilliant', 'astronomical',]
 
+export function randomWord(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+// user fill in
+// you name
+// descriptive
+// friend name
+// idol name
+//
+//
+//
+//
+//
+//
