721234e0efe49316ec8a5c56eba1a626d1b10068
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-31 14:44:53 -0600

    changed intro

--- src/Components/home/Intro.js
+++ src/Components/home/Intro.js
@@ -5,9 +5,9 @@ export default function Intro() {
     <Card>
       <Card.Body className='card'>
         <h6>You want to drink, I want you to drink, learn about space and laugh!</h6>
-        <p>Let's make this quick, fill in the blanks and click submit! You will
-          get a funny story, or at least I hope it's funny '[insert chuckle]!'
-          You will also get two cards with info about your drink and a space object.</p>
+        <p>Let's make this quick, fill in the blanks and click "Lets's do this!" You will
+          get a funny story, or at least I hope it's funny haha! Then you will get two 
+          cards with info about your drink and a space object.</p>
         <p>Then feel free to explore the Drinks and Space sections to learn more 
           about random space objects and get random drink ideas!</p>
       </Card.Body>
