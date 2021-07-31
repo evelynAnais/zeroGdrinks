2c5328e54dde78bc25dd72abff691a6ecab398c4
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-31 15:52:59 -0600

    formatting

--- src/Components/home/Intro.js
+++ src/Components/home/Intro.js
@@ -2,15 +2,17 @@ import { Card } from 'react-bootstrap'
 
 export default function Intro() {
   return (
-    <Card>
-      <Card.Body className='card'>
-        <h6>You want to drink, I want you to drink, learn about space and laugh!</h6>
-        <p>Let's make this quick, fill in the blanks and click "Lets's do this!" You will
-          get a funny story, or at least I hope it's funny haha! Then you will get two 
-          cards with info about your drink and a space object.</p>
-        <p>Then feel free to explore the Drinks and Space sections to learn more 
-          about random space objects and get random drink ideas!</p>
-      </Card.Body>
-    </Card>
+    <div className='container'>
+      <Card>
+        <Card.Body className='card'>
+          <h6>You want to drink, I want you to drink, learn about space and laugh!</h6>
+          <p>Let's make this quick, fill in the blanks and click "Lets's do this!" You will
+            get a funny story, or at least I hope it's funny haha! Then you will get two 
+            cards with info about your drink and a space object.</p>
+          <p>Then feel free to explore the Drinks and Space sections to learn more 
+            about random space objects and get random drink ideas!</p>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
