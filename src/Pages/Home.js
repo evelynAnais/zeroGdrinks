dc062f0adbd2cd9e3523642e07653e325ff41a8a
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-31 21:33:38 -0600

    button to reset story

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -26,6 +26,8 @@ export default function Home() {
   }
   useEffect(object, [])
 
+  const reset = () => setStory(null)
+
   return (
     <>{!story
       ?
@@ -40,6 +42,12 @@ export default function Home() {
         <Story drink={drink.drinks[0]} spaceObject={spaceObject} story={story} />
         <SpaceCard spaceObject={spaceObject} />
         <DrinkCard drink={drink.drinks[0]} />
+        <div className='container'>
+          <div className='row d-flex justify-content-around'>
+            <button type='button' className="btn btn-dark" onClick={reset}>Start Again! Because, why not!</button>
+          </div>
+        </div>
+        
       </>
       }
     </>
